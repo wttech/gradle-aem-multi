912c545905a494c059e0cd9f0f4c4ee719aa8e0d
krystian.panek <krystian.panek@cognifide.com>
2017-06-12 09:56:35 +0200

    Structure proposal / to be fixed

---
+++ app/core/src/main/kotlin/com/company/aem/example/core/components/page/PageDetailsServlet.kt
@@ -0,0 +1,32 @@
+package com.company.aem.example.core.components.page
+
+import com.day.cq.wcm.api.NameConstants
+import com.company.aem.example.common.JsonUtils
+import com.company.aem.example.core.components.page.PageModel
+import org.apache.sling.api.SlingHttpServletRequest
+import org.apache.sling.api.SlingHttpServletResponse
+import org.apache.sling.api.servlets.SlingAllMethodsServlet
+import org.osgi.service.component.annotations.Component
+import javax.servlet.Servlet
+
+/**
+ * @see <http://localhost:4502/content/geometrixx-outdoors/en.details.json>
+ */
+@Component(
+        service = arrayOf(Servlet::class),
+        property = arrayOf(
+                "sling.servlet.extensions=json",
+                "sling.servlet.selectors=details",
+                "sling.servlet.resourceTypes=${NameConstants.NT_PAGE}"
+        )
+)
+class PageDetailsServlet : SlingAllMethodsServlet() {
+
+    override fun doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse) {
+        val page = request.resource.adaptTo(PageModel::class.java)
+        val json = JsonUtils.GSON.toJson(page)
+
+        response.writer.use { it.write(json) }
+    }
+
+}
---
+++ app/core/src/test/java/com/company/aem/example/core/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package com.company.aem.example.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleUnitTest {
+
+    @Test
+    public void someTest() {
+        assertTrue(true);
+    }
+
+}
