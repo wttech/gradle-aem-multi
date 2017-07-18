e8207ae2bd89ab34ba84813b645593a591d30602
krystian.panek <krystian.panek@cognifide.com>
2017-07-18 15:28:11 +0200

    SCSS compilation works using webpack

--- app/design/src/main/content/jcr_root/etc/designs/example/publish/src/webpack.config.js
+++ app/design/src/main/content/jcr_root/etc/designs/example/publish/src/webpack.config.js
@@ -1,13 +1,66 @@
-const webpack = require('webpack');
-const path = require('path');
+const Webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './js/main.js',
-    output: {
-        path: './../dist',
-        filename: 'main.bundle.js'
+module.exports = [
+    {
+        entry: [
+            __dirname + '/js/main.js',
+        ],
+        output: {
+            path: __dirname + '/../dist',
+            filename: 'main.bundle.js'
+        },
+        plugins: [
+            new Webpack.optimize.UglifyJsPlugin(),
+        ]
     },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin()
-    ]
-};
+    {
+        entry: [
+            __dirname + '/css/main.scss',
+        ],
+        output: {
+            path: __dirname + '/../dist',
+            filename: 'main.css'
+        },
+        module: {
+            rules: [
+                {
+                    test: /(\.css|\.scss)$/,
+                    use: ExtractTextPlugin.extract({
+                        fallback: "style-loader",
+                        use: [
+                            {
+                                loader: "css-loader",
+                                options: {
+                                    sourceMap: true,
+                                    modules: true,
+                                    importLoaders: true,
+                                    localIdentName: "[name]__[local]___[hash:base64:5]"
+                                }
+                            },
+                            // {
+                            //     loader: "postcss-loader",
+                            //     options: {
+                            //         plugins: function () {
+                            //             return [
+                            //                 require("autoprefixer")
+                            //             ];
+                            //         }
+                            //     }
+                            // },
+                            {
+                                loader: "sass-loader",
+                                options: {
+                                    sourceMap: true
+                                }
+                            }
+                        ]
+                    })
+                }
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin('[name].css')
+        ]
+    }
+];
