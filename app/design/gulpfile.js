d8ff0b837376ce59261d71c808ed6c4f22d2785e
krystian.panek <krystian.panek@cognifide.com>
2017-07-18 10:13:09 +0200

    Design SCSS compilation with autoprefixer and concatenation working properly

--- app/design/gulpfile.js
+++ app/design/gulpfile.js
@@ -1,11 +1,31 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
 
-gulp.task('default', ['build-author', 'build-publish'], function() {});
+var clientlibsRoot = './src/main/content/jcr_root/etc/designs/example';
 
-gulp.task('build-author', function() {
-    console.log('Build author!');
-});
+function setupClientlib(name) {
+    var root = clientlibsRoot + '/' + name;
+    var styles = name + '-styles';
+    var scripts = name + '-scripts';
 
-gulp.task('build-publish', function() {
-    console.log('Build publish!');
-});
+    gulp.task(styles, function () {
+        return gulp.src(root + '/scss/**/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(autoprefixer())
+            .pipe(concat('main.css'))
+            .pipe(gulp.dest(root + '/dist'));
+    });
+
+    gulp.task(scripts, function () {
+        // TODO ...
+    });
+
+    gulp.task(name, [styles, scripts], function() {});
+}
+
+setupClientlib('author');
+setupClientlib('publish');
+
+gulp.task('default', ['author', 'publish'], function() {});
