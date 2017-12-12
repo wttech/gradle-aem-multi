ca3e1ab4f9651f8117e4b442522efb01ec3df9a1
krystian.panek <krystian.panek@cognifide.com>
2017-12-12 08:12:44 +0100

    AEM 6.3 ready

--- app/core/src/main/kotlin/com/company/aem/example/core/components/page/PageDetailsServlet.kt
+++ app/core/src/main/kotlin/com/company/aem/example/core/components/page/PageDetailsServlet.kt
@@ -3,6 +3,7 @@ package com.company.aem.example.core.components.page
 import com.company.aem.example.common.JsonUtils
 import com.day.cq.commons.jcr.JcrConstants
 import com.day.cq.wcm.api.NameConstants
+import com.google.common.net.MediaType
 import org.apache.sling.api.SlingHttpServletRequest
 import org.apache.sling.api.SlingHttpServletResponse
 import org.apache.sling.api.servlets.SlingAllMethodsServlet
@@ -23,10 +24,10 @@ import javax.servlet.Servlet
 class PageDetailsServlet : SlingAllMethodsServlet() {
 
   override fun doGet(request: SlingHttpServletRequest, response: SlingHttpServletResponse) {
-    val page = request.resource.getChild(JcrConstants.JCR_CONTENT).adaptTo(PageModel::class.java)
-    val json = JsonUtils.GSON.toJson(page)
+    val page = request.resource.getChild(JcrConstants.JCR_CONTENT)?.adaptTo(PageModel::class.java)
 
-    response.writer.use { it.write(json) }
+    response.contentType = MediaType.JSON_UTF_8.toString()
+    response.writer.use { it.write(JsonUtils.GSON.toJson(page)) }
   }
 
 }
