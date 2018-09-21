602f65a4fe3992e6c7e33615591e9b2ed2a8533e
Tomasz Paulus <tomasz.paulus@cognifide.com>
2018-09-21 15:09:27 +0200

    modularising webpack modules

--- app/design/src/main/content/jcr_root/etc/designs/example/publish/src/webpack.config.js
+++ app/design/src/main/content/jcr_root/etc/designs/example/publish/src/webpack.config.js
@@ -1,42 +1,38 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production'
 const path = require('path');
+const { isProduction } = require('webpack-mode');
+const merge = require('webpack-merge');
+const modules = require('./webpack/index');
+const glob = require('glob');
+
+const entries = [
+  ...glob.sync('./js/**/*.js'),
+  ...glob.sync('./styles/**/*.scss'),
+];
 
 const config = {
-  sourceJs: __dirname + '/js/main.js',
-  sourceScss: __dirname + '/styles/main.scss',
-  distPath: __dirname + '/../dist'
+  images: path.join(__dirname, 'assets/img'),
+  fonts: path.join(__dirname, 'assets/fonts'),
+  js: path.join(__dirname, 'js'),
+  sass: path.join(__dirname, 'styles'),
+  startPath: '',
+  devServerPort: '3000'
 };
 
-module.exports = [
-  {
-    entry: [
-      config.sourceJs
-    ],
-    output: {
-      path: config.distPath,
-    },
-    entry: [
-      config.sourceScss
-    ],
-    output: {
-      path: config.distPath,
-    },
-    module: {
-      rules: [{
-        test: /\.s?css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          "sass-loader"
-        ]
-      }]
-    },
-    plugins: [
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
-      })
-    ]
-  }
-];
+const common = {
+  devtool: isProduction ? 'none' : 'source-map',
+  entry: entries,
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+};
+
+/* eslint-disable comma-dangle */
+
+module.exports = merge(
+  common,
+  modules.production,
+  modules.styles(config.sass, isProduction)
+);
+
+/* eslint-enable */
---
+++ app/design/src/main/content/jcr_root/etc/designs/example/publish/src/webpack/index.js
@@ -0,0 +1,5 @@
+const styles = require('./modules/styles');
+
+module.exports = {
+    styles
+};
