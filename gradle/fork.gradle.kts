00117b9750ac947f42b1a7a750c1c599269fe698
krystian.panek <krystian.panek@cognifide.com>
2019-01-15 12:12:32 +0100

    Fork impr

--- gradle/fork.gradle.kts
+++ gradle/fork.gradle.kts
@@ -3,13 +3,26 @@ import com.neva.gradle.fork.ForkExtension
 configure<ForkExtension> {
     properties {
         define(mapOf(
-                "projectGroup" to { validator { shouldNotContain("projectName") } },
-
-                "aemInstanceType" to { select("local", "remote") },
+                "projectName" to { description = "Artifact 'name' coordinate (lowercase)" },
+                "projectLabel" to { description = "Nice project name (human-readable)" },
+                "projectGroup" to {
+                    validator { javaPackage(); notContains("projectName") }
+                    description = "Java package in source code and artifact 'group' coordinate"
+                },
+                "aemInstanceType" to {
+                    select("local", "remote")
+                    description = "Local - instance will be created on local file system.\nRemote - connecting to remote instance only."
+                },
                 "aemInstanceRunModes" to { text("nosamplecontent") },
                 "aemInstanceJvmOpts" to { text("-server -Xmx1024m -XX:MaxPermSize=256M -Djava.awt.headless=true") },
-                "aemInstanceAuthorHttpUrl" to { url("http://localhost:4502") },
-                "aemInstancePublishHttpUrl" to { url("http://localhost:4503") }
+                "aemInstanceAuthorHttpUrl" to {
+                    url("http://localhost:4502")
+                    description = "URL for accessing AEM author instance"
+                },
+                "aemInstancePublishHttpUrl" to {
+                    url("http://localhost:4503")
+                    description = "URL for accessing AEM publish instance"
+                }
         ))
     }
     config {
