93363814b79ae29adcdef24d4b183f9add2f7c97
krystian.panek <krystian.panek@cognifide.com>
2019-01-20 08:34:41 +0100

    Optional fields

--- gradle/fork.gradle.kts
+++ gradle/fork.gradle.kts
@@ -10,20 +10,23 @@ configure<ForkExtension> {
                 "projectLabel" to { description = "Nice project name (human-readable)" },
                 "projectGroup" to {
                     description = "Java package in source code and artifact 'group' coordinate"
-                    validator { javaPackage(); notContains("projectName") }
+                    validator { javaPackage(); notEndsWith("projectName") }
                 },
                 "aemInstanceType" to {
                     select("local", "remote")
                     description = "local - instance will be created on local file system.\nremote - connecting to remote instance only."
+                    controller { toggle(value == "local", "aemInstanceRunModes", "aemInstanceJvmOpts", "aemLocalInstance*") }
                 },
                 "aemInstanceRunModes" to { text("nosamplecontent") },
                 "aemInstanceJvmOpts" to { text("-server -Xmx1024m -XX:MaxPermSize=256M -Djava.awt.headless=true") },
                 "aemInstanceAuthorHttpUrl" to {
                     url("http://localhost:4502")
+                    optional()
                     description = "URL for accessing AEM author instance"
                 },
                 "aemInstancePublishHttpUrl" to {
                     url("http://localhost:4503")
+                    optional()
                     description = "URL for accessing AEM publish instance"
                 }
         ))
