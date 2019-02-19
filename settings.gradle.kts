ea8bf3616fa0f8417df36d0c91d11354c36b197c
mierzwid <mierzwid@gmail.com>
2019-02-19 15:43:26 +0100

    #78 - renaming content project to site to better reflect their new meaning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,8 @@ rootProject.name = "example"
 include("aem")
 include("aem:common")
 include("aem:sites")
-include("aem:content.init")
-include("aem:content.demo")
+include("aem:site.init")
+include("aem:site.demo")
 include("aem:migration")
 
 include("aem:assembly:app")
