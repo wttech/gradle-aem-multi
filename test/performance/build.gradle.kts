dfca794bcedaf6c9f1666ec03fa0232c399148b4
krystian.panek <krystian.panek@cognifide.com>
2019-09-17 09:25:05 +0200

    Performance tests suites

--- test/performance/build.gradle.kts
+++ test/performance/build.gradle.kts
@@ -1,15 +1,37 @@
-import com.moowork.gradle.node.npm.NpmTask
-
-description = "Example - Performance Tests"
+import com.moowork.gradle.node.yarn.YarnTask
 
 plugins {
+    id("com.cognifide.aem.common")
     id("com.moowork.node")
 }
 
-tasks {
-    register<NpmTask>("test") {
-        dependsOn("npmInstall")
-        setNpmCommand("run")
-        setArgs(listOf("lighthouse"))
+description = "Example - Performance Tests"
+
+aem {
+    tasks {
+        register<YarnTask>("run") {
+            dependsOn("yarn")
+            group = "check"
+
+            val baseUrl = props.string("test.baseUrl") ?: main.environment.hosts.publish.url
+            val reportsDir = file("build/reports")
+            val configTpl = file("sites/${baseUrl.substringAfter("://")}.conf")
+                    .takeIf { it.exists() } ?: file("sites/default.conf")
+            val configFile = file("build/sites/${configTpl.name}")
+
+            setWorkingDir(projectDir)
+            setYarnCommand("lighthouse-batch")
+            setArgs(listOf("--file", "$configFile", "--html", "--out", "$reportsDir"))
+
+            doFirst {
+                configFile.parentFile.mkdirs()
+                configFile.printWriter().use {writer ->
+                    configTpl.forEachLine { path ->
+                        writer.println("$baseUrl$path")
+                    }
+                }
+
+            }
+        }
     }
 }
