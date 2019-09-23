6b5db3286d01f67631ae78a005b5166e0c8b1571
krystian.panek <krystian.panek@cognifide.com>
2019-09-23 09:33:07 +0200

    Multi site lighthouse perf test with thresholds

--- test/performance/build.gradle.kts
+++ test/performance/build.gradle.kts
@@ -1,5 +1,10 @@
 import com.moowork.gradle.node.yarn.YarnExecRunner
 
+class CliConfig {
+    var paths = listOf<String>()
+    var args = listOf<String>()
+}
+
 plugins {
     id("com.cognifide.aem.common")
     id("com.moowork.node")
@@ -14,15 +19,20 @@ aem {
             group = "check"
             description = "Run performance tests (Lighthouse)"
 
-            val baseUrl = props.string("test.baseUrl") ?: main.environment.hosts.publish.url
-            val configName = "${baseUrl.substringAfter("://")}.conf"
-            val configFile = file("sites/$configName")
-                    .takeIf { it.exists() } ?: file("sites/default.conf")
-            val reportDir = file("build/lighthouse")
-
             doLast {
+                val baseUrl = props.string("test.baseUrl") ?: main.environment.hosts.publish.url
+                val configName = baseUrl.substringAfter("://")
+
+                val cliConfig = (file("sites/$configName/cli.json")
+                        .takeIf { it.exists() } ?: file("sites/default/cli.json")).run {
+                    formats.fromJson(readText(), CliConfig::class.java)
+                }
+                val lighthouseConfig = file("sites/$configName/lighthouse.json")
+                        .takeIf { it.exists() } ?: file("sites/default/lighthouse.json")
+                val reportDir = file("build/lighthouse")
+
                 reportDir.mkdirs()
-                configFile.forEachLine { path ->
+                cliConfig.paths.forEach { path ->
                     if (path.isNotBlank()) {
                         val url = "$baseUrl$path"
                         val fileName = url.substringAfter("://").replace("/", "_")
@@ -31,8 +41,8 @@ aem {
                             workingDir = projectDir
                             arguments = mutableListOf(
                                     "lighthouse-ci", url, "--report=$reportDir",
-                                    "--filename=$fileName", "--config-path=lighthouse.json"
-                            )
+                                    "--filename=$fileName", "--config-path=$lighthouseConfig"
+                            ) + cliConfig.args
                             execute()
                         }
                     }
