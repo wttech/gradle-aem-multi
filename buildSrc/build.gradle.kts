2cf55cbec0ad875872fc219133879982a2d45873
krystian.panek <krystian.panek@cognifide.com>
2019-11-04 12:48:06 +0100

    OakPAL done

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:8.0.1")
+    implementation("com.cognifide.gradle:aem-plugin:8.1.0")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
     implementation("com.neva.gradle:fork-plugin:3.1.6")
     implementation("com.github.node-gradle:gradle-node-plugin:2.1.1")
