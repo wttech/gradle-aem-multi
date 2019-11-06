e4f6d55aadf20f11c8859e3dab902f7d49242fa4
Krystian Panek <krystian.panek@cognifide.com>
2019-11-06 10:14:36 +0100

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     implementation("com.cognifide.gradle:aem-plugin:8.1.0")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
-    implementation("com.neva.gradle:fork-plugin:3.1.6")
+    implementation("com.neva.gradle:fork-plugin:3.1.7")
     implementation("com.github.node-gradle:gradle-node-plugin:2.1.1")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
 }
