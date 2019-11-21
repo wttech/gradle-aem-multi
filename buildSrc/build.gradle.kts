80c2b016a69c95fdd57fee3c885130969fda6a41
krystian.panek <krystian.panek@cognifide.com>
2019-11-21 11:01:36 +0100

    GAP upgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,9 +15,9 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:9.0.2")
+    implementation("com.cognifide.gradle:aem-plugin:9.0.7")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
-    implementation("com.neva.gradle:fork-plugin:4.0.0")
+    implementation("com.neva.gradle:fork-plugin:4.0.1")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.50")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.4")
