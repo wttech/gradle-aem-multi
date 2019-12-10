2245c7ca402b843808b1904bd1dd093af30f498e
Krystian Panek <krystian.panek@cognifide.com>
2019-12-10 14:38:26 +0100

    Duckrails poc

--- aem/build.gradle.kts
+++ aem/build.gradle.kts
@@ -30,6 +30,11 @@ aem {
                         watchConfigDir("conf")
                     }
                 }
+                "duckrails" {
+                    resolve {
+                        ensureDir()
+                    }
+                }
             }
         }
         hosts {
@@ -69,7 +74,8 @@ aem {
             packages {
                 // "dep.vanity-urls"("pkg/vanityurls-components-1.0.2.zip")
                 "dep.kotlin"("org.jetbrains.kotlin:kotlin-osgi-bundle:${Build.KOTLIN_VERSION}")
-                "dep.acs-aem-commons"("https://github.com/Adobe-Consulting-Services/acs-aem-commons/releases/download/acs-aem-commons-4.0.0/acs-aem-commons-content-4.0.0-min.zip")
+                "dep.core-components-all"("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
+                "dep.core-components-examples"("com.adobe.cq:core.wcm.components.examples:2.8.0@zip")
                 "tool.ac-tool"("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool", "accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip", "accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip")
                 "tool.aem-easy-content-upgrade"("https://github.com/valtech/aem-easy-content-upgrade/releases/download/2.0.0/aecu.bundle-2.0.0.zip")
                 "tool.search-webconsole-plugin"("com.neva.felix:search-webconsole-plugin:1.2.0")
