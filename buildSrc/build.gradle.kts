d9cdf7ed2e9c6bfbebc1b9270c34c0f0c2052a75
Krystian Panek <krystian.panek@cognifide.com>
2019-12-21 21:52:17 +0100

    Bum#

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     implementation("com.cognifide.gradle:aem-plugin:10.0.0")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
-    implementation("com.neva.gradle:fork-plugin:4.1.1")
+    implementation("com.neva.gradle:fork-plugin:4.1.2")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.4")
