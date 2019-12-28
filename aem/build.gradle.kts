0121714071d990a5efb625200dfff3824724d4ec
Krystian Panek <krystian.panek@cognifide.com>
2019-12-28 16:28:16 +0100

    Docs

--- aem/build.gradle.kts
+++ aem/build.gradle.kts
@@ -51,6 +51,7 @@ aem {
     localInstance {
         install {
             files {
+                //useLocal("/Users/krystian.panek/Projects/mapper/fragment/sling-mapper-extender.jar")
                 // https://github.com/Cognifide/gradle-aem-plugin#pre-installed-osgi-bundles-and-crx-packages
             }
         }
