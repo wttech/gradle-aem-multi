8014ba023128e7a0b2a9810bc2c77619cdb42f3b
Krystian Panek <krystian.panek@cognifide.com>
2020-01-13 20:10:57 +0100

    GAP bump

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:10.0.1")
+    implementation("com.cognifide.gradle:aem-plugin:10.1.0")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
     implementation("com.neva.gradle:fork-plugin:4.2.0")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.0")
