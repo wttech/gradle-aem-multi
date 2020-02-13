7e8428316035f19fabdc9f6a35a445b9250f793b
Krystian Panek <krystian.panek@cognifide.com>
2020-02-13 08:43:27 +0100

    Minor

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,17 +6,16 @@ repositories {
     mavenLocal()
     jcenter()
     gradlePluginPortal()
-    maven("http://dl.bintray.com/cognifide/maven-public")
-    maven("https://dl.bintray.com/neva-dev/maven-public")
+    maven("https://dl.bintray.com/neva-dev/maven-public") // TODO to be removed
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+    implementation("com.neva.gradle:fork-plugin:4.2.0")
+    implementation("com.netflix.nebula:nebula-project-plugin:7.0.4")
     implementation("com.cognifide.gradle:environment-plugin:1.0.0")
     implementation("com.cognifide.gradle:aem-plugin:11.0.0")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
-    implementation("com.neva.gradle:fork-plugin:4.2.0")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.0")
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
-    implementation("com.netflix.nebula:nebula-project-plugin:7.0.4")
 }
