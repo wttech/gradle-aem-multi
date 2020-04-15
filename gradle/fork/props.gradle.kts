7e8b2cc3e9c952b1dcd205afe136a908b17bedec
Krystian Panek <krystian.panek@cognifide.com>
2020-04-15 23:09:19 +0200

    User options on props dialog

--- gradle/fork/props.gradle.kts
+++ gradle/fork/props.gradle.kts
@@ -16,26 +16,6 @@ configure<ForkExtension> {
                 }
         ))
 
-        define("Authorization", mapOf(
-                "companyUser" to {
-                    label = "User"
-                    description = "Authorized to access AEM files"
-                    defaultValue = System.getProperty("user.name").orEmpty()
-                    optional()
-                },
-                "companyPassword" to {
-                    label = "Password"
-                    description = "For above user"
-                    optional()
-                },
-                "companyDomain" to {
-                    label = "Domain"
-                    description = "Needed only when accessing AEM files over SMB"
-                    defaultValue = System.getenv("USERDOMAIN").orEmpty()
-                    optional()
-                }
-        ))
-
         define("Instance", mapOf(
                 "instanceType" to {
                     label = "Type"
@@ -64,6 +44,16 @@ configure<ForkExtension> {
                     label = "Publish Only"
                     description = "Limits instances to work with to publish instance only."
                     checkbox(false)
+                },
+                "instanceSatisfierEnabled" to {
+                    label = "Satisfier Enabled"
+                    description = "Turns on/off automated package pre-installation."
+                    checkbox(true)
+                },
+                "instanceProvisionerEnabled" to {
+                    label = "Provisioner Enabled"
+                    description = "Turns on/off automated instance configuration."
+                    checkbox(true)
                 }
         ))
 
@@ -91,39 +81,61 @@ configure<ForkExtension> {
                     description = "For directory containing backup files. Protocols supported: SMB/SFTP"
                     optional()
                 },
-                "instanceRunModes" to {
+                "localInstanceRunModes" to {
                     label = "Run Modes"
                     text("local")
                 },
-                "instanceJvmOpts" to {
+                "localInstanceJvmOpts" to {
                     label = "JVM Options"
                     text("-server -Xmx2048m -XX:MaxPermSize=512M -Djava.awt.headless=true")
                 }
         ))
 
-        define("Other", mapOf(
-                "instanceSatisfierEnabled" to {
-                    label = "Instance Satisfier Enabled"
-                    description = "Turns on/off automated package pre-installation."
-                    checkbox(true)
-                },
-                "instanceProvisionerEnabled" to {
-                    label = "Instance Provisioner Enabled"
-                    description = "Turns on/off automated instance configuration."
-                    checkbox(true)
-                },
-                "packageValidatorEnabled" to {
-                    label = "Package Validator Enabled"
-                    description = "Turns on/off package validation using OakPAL."
-                    checkbox(true)
-                },
+        define("Package", mapOf(
                 "packageDamAssetToggle" to {
-                    label = "Package Deploy Without DAM Worklows"
+                    label = "Deploy Without DAM Worklows"
                     description = "Turns on/off temporary disablement of assets processing for package deployment time.\n" +
                             "Useful to avoid redundant rendition generation when package contains renditions synchronized earlier."
                     checkbox(true)
                 },
+                "packageValidatorEnabled" to {
+                    label = "Validator Enabled"
+                    description = "Turns on/off package validation using OakPAL."
+                    checkbox(true)
+                },
+                "packageNestedValidation" to {
+                    label = "Nested Validation"
+                    description = "Turns on/off separate validation of built subpackages."
+                    checkbox(true)
+                },
+                "packageBundleTest" to {
+                    label = "Bundle Test"
+                    description = "Turns on/off running tests for built bundles put under install path."
+                    checkbox(true)
+                }
+        ))
 
+        define("Authorization", mapOf(
+                "companyUser" to {
+                    label = "User"
+                    description = "Authorized to access AEM files"
+                    defaultValue = System.getProperty("user.name").orEmpty()
+                    optional()
+                },
+                "companyPassword" to {
+                    label = "Password"
+                    description = "For above user"
+                    optional()
+                },
+                "companyDomain" to {
+                    label = "Domain"
+                    description = "Needed only when accessing AEM files over SMB"
+                    defaultValue = System.getenv("USERDOMAIN").orEmpty()
+                    optional()
+                }
+        ))
+
+        define("Other", mapOf(
                 "notifierEnabled" to {
                     label = "Notifications"
                     description = "Controls displaying of GUI notifications (baloons)"
