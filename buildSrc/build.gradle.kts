c3cd63b1b81e93c578724343d48393e27812eae3
Krystian Panek <krystian.panek@cognifide.com>
2020-04-15 11:16:06 +0200

    Minor

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
     implementation("com.neva.gradle:fork-plugin:4.2.0")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.4")
-    implementation("com.cognifide.gradle:environment-plugin:0.1.10")
+    implementation("com.cognifide.gradle:environment-plugin:0.1.12")
     implementation("com.cognifide.gradle:aem-plugin:13.0.0")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
