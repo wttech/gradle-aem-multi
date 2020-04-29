fc5ec9a576ed907d302f9ea03589a1bd7d957454
Krystian Panek <krystian.panek@cognifide.com>
2020-04-29 13:12:54 +0200

    Fix

--- gradle/fork/props.gradle.kts
+++ gradle/fork/props.gradle.kts
@@ -129,6 +129,7 @@ configure<ForkExtension> {
                     description = "Turns on/off temporary disablement of assets processing for package deployment time.\n" +
                             "Useful to avoid redundant rendition generation when package contains renditions synchronized earlier."
                     checkbox(true)
+                    dynamic()
                 }
         ))
 
