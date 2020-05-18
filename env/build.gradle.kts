19ab1dcd60df4c7ac23742bd0f10a2fef8e50d28
Krystian Panek <krystian.panek@cognifide.com>
2020-05-18 21:57:13 +0200

    Provision instead satisfy

--- env/build.gradle.kts
+++ env/build.gradle.kts
@@ -16,19 +16,13 @@ aem {
     }
 
     instance {
-        satisfier { // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/instance-plugin.md#task-instancesatisfy
-            packages {
-                // "dep.vanity-urls"("pkg/vanityurls-components-1.0.2.zip")
-                "dep.core-components-all"("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
-                "dep.core-components-examples"("com.adobe.cq:core.wcm.components.examples:2.8.0@zip")
-                "tool.aem-easy-content-upgrade"("https://github.com/valtech/aem-easy-content-upgrade/releases/download/3.1.0/aecu.bundle-3.1.0.zip")
-                "tool.ac-tool.package"("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip")
-                "tool.ac-tool.oak-index"("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip")
-                "tool.search-webconsole-plugin"("com.neva.felix:search-webconsole-plugin:1.3.0")
-            }
-        }
-        provisioner {
-            // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/instance-plugin.md#task-instanceprovision
+        provisioner { // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/instance-plugin.md#task-instanceprovision
+            deployPackage("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
+            deployPackage("com.adobe.cq:core.wcm.components.examples:2.8.0@zip")
+            deployPackage("https://github.com/valtech/aem-easy-content-upgrade/releases/download/3.1.0/aecu.bundle-3.1.0.zip")
+            deployPackage("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip")
+            deployPackage("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip")
+            deployPackage("com.neva.felix:search-webconsole-plugin:1.3.0")
         }
     }
 
@@ -93,7 +87,7 @@ tasks {
         dependsOn(":requireProps")
     }
     environmentUp {
-        mustRunAfter(instanceUp, instanceSatisfy, instanceProvision, instanceSetup)
+        mustRunAfter(instanceUp, instanceProvision, instanceSetup)
     }
     environmentAwait {
         mustRunAfter(instanceAwait)
