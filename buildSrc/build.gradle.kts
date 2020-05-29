4670380d87f894844b5eb2ed7a4a143fc35db611
Krystian Panek <krystian.panek@cognifide.com>
2020-05-29 08:50:29 +0200

    Bump

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("com.neva.gradle:fork-plugin:5.0.1")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
     implementation("com.cognifide.gradle:environment-plugin:0.1.13")
-    implementation("com.cognifide.gradle:aem-plugin:14.0.0")
+    implementation("com.cognifide.gradle:aem-plugin:14.0.1")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.3")
