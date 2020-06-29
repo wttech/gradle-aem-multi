9d9f44475f279e3fa7a1eb949ebe4ef1f11b3552
Krystian Panek <krystian.panek@cognifide.com>
2020-06-29 09:36:02 +0200

    Fix for Windows / props dialog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,7 +6,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
-    implementation("com.neva.gradle:fork-plugin:5.0.2")
+    implementation("com.neva.gradle:fork-plugin:5.0.3")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
     implementation("com.cognifide.gradle:environment-plugin:1.0.0")
     implementation("com.cognifide.gradle:aem-plugin:14.0.2")
