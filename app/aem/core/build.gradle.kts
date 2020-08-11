bdfb1b109dd86bce8a0788323259b1942a883970
Krystian Panek <krystian.panek@cognifide.com>
2020-08-11 09:05:27 +0200

    Update to AEM archetype 23

--- app/aem/core/build.gradle.kts
+++ app/aem/core/build.gradle.kts
@@ -9,7 +9,8 @@ apply(from = rootProject.file("app/aem/common.gradle.kts"))
 description = "Example - Core"
 
 dependencies {
-    testImplementation("uk.org.lidalia:slf4j-test:1.1.0")
+    testImplementation("uk.org.lidalia:slf4j-test:1.0.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.4.1")
     testImplementation("org.mockito:mockito-core:2.25.1")
     testImplementation("org.mockito:mockito-junit-jupiter:2.25.1")
     testImplementation("junit-addons:junit-addons:1.4")
