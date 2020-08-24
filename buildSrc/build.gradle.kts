b23440cd1dc838715b0d59fd0fe9d760aab4f3c5
Krystian Panek <krystian.panek@cognifide.com>
2020-08-24 21:47:08 +0200

    Hotfix

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation("com.neva.gradle:fork-plugin:5.0.5")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
     implementation("com.cognifide.gradle:environment-plugin:1.0.2")
-    implementation("com.cognifide.gradle:aem-plugin:14.2.8")
+    implementation("com.cognifide.gradle:aem-plugin:14.2.9")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
     implementation("com.cognifide.gradle:lighthouse-plugin:1.0.0")
     implementation("com.github.node-gradle:gradle-node-plugin:2.2.4")
