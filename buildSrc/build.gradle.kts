f20ee851f5dcfbcd425a3916222c0c70ebe7ba19
mierzwid <mierzwid@gmail.com>
2020-10-28 06:59:10 +0100

    upgrading environment plugin & Gradle version

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.0")
     implementation("com.neva.gradle:fork-plugin:5.0.5")
     implementation("com.netflix.nebula:nebula-project-plugin:7.0.9")
-    implementation("com.cognifide.gradle:environment-plugin:1.0.2")
+    implementation("com.cognifide.gradle:environment-plugin:1.1.3")
     implementation("com.cognifide.gradle:aem-plugin:14.4.6")
     implementation("com.cognifide.gradle:htl-plugin:1.0.0")
     implementation("org.apache.sling:org.apache.sling.caconfig.bnd-plugin:1.0.2")
