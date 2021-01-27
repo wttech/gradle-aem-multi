b1621aebb15c218c8c4b4bde0f4f27148e7a1abc
Bartosz Zbytniewski <bartosz.zbytniewski.dev@gmail.com>
2021-01-27 14:24:42 +0100

    fix: Remove Cognifide references, update logo

--- env/build.gradle.kts
+++ env/build.gradle.kts
@@ -10,17 +10,17 @@ repositories {
 
 aem {
     `package` {
-        validator { // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/package-plugin.md#crx-package-validation
+        validator { // https://github.com/wttech/gradle-aem-plugin/blob/master/docs/package-plugin.md#crx-package-validation
             base("com.adobe.acs:acs-aem-commons-oakpal-checks:4.4.0")
         }
     }
 
     instance {
-        provisioner { // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/instance-plugin.md#task-instanceprovision
+        provisioner { // https://github.com/wttech/gradle-aem-plugin/blob/master/docs/instance-plugin.md#task-instanceprovision
             enableCrxDe()
             deployPackage("com.adobe.cq:core.wcm.components.all:2.8.0@zip")
             deployPackage("https://github.com/valtech/aem-easy-content-upgrade/releases/download/3.1.1/aecu.bundle-3.1.1.zip")
-            deployPackage("https://github.com/Cognifide/aem-stubs/releases/download/1.0.3/stubs-app-1.0.3.zip")
+            deployPackage("https://github.com/wttech/aem-stubs/releases/download/1.0.3/stubs-app-1.0.3.zip")
             deployPackage("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip")
             deployPackage("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool/accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip")
             deployPackage("com.neva.felix:search-webconsole-plugin:1.3.0")
@@ -30,13 +30,13 @@ aem {
     localInstance {
         install {
             files {
-                // https://github.com/Cognifide/gradle-aem-plugin/blob/master/docs/local-instance-plugin.md#pre-installed-osgi-bundles-and-crx-packages
+                // https://github.com/wttech/gradle-aem-plugin/blob/master/docs/local-instance-plugin.md#pre-installed-osgi-bundles-and-crx-packages
             }
         }
     }
 }
 
-environment { // https://github.com/Cognifide/gradle-environment-plugin
+environment { // https://github.com/wttech/gradle-environment-plugin
     docker {
         containers {
             "httpd" {
